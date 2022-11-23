dece05546dde5842033ec3d7c1065b8824c98a02
root <juneho.bang85@gmail.com>
2022-11-23 14:53:57 +0900

    first predictive parser

---
+++ examples/fig_02_27.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type Parser struct {
+	eq        string
+	lookahead int
+}
+type MyError struct{}
+
+func (m *MyError) Error() string {
+	return "syntax error"
+}
+
+func (p Parser) initialize() {
+	p.lookahead = 0
+	p.eq = ""
+}
+
+func (p Parser) getEquation() {
+	fmt.Scanln(&p.eq)
+}
+
+func (p Parser) expr() error {
+	p.term()
+	for j := 1; j > 0; j++ {
+		if p.lookahead == len(p.eq) {
+			return nil
+		} else if p.eq[p.lookahead] == '+' {
+
+		} else if p.eq[p.lookahead] == '-' {
+
+		} else {
+			return fmt.Errorf("syntax error")
+		}
+	}
+}
+
+func (p Parser) term() error {
+	if _, err := strconv.Atoi(string(p.eq[p.lookahead])); err != nil {
+		return fmt.Errorf("syntax error")
+	}
+	fmt.Println(p.eq[p.lookahead])
+	p.match(string(p.eq[p.lookahead]))
+	return nil
+}
+
+func (p Parser) match(c string) error {
+	if c != string(p.eq[p.lookahead]) {
+		return fmt.Errorf("syntax error")
+	}
+	p.lookahead++
+	return nil
+}
+
+func main() {
+	var p Parser
+	p.initialize()
+	p.getEquation()
+	p.expr()
+	fmt.Println('\n')
+}
