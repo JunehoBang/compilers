1419c9eeb09898e54dcf57df2243d67b43cdf847
root <juneho.bang85@gmail.com>
2022-11-24 14:32:42 +0900

    An example of post-fix equation generator that were implemented as a predictive-parser

--- examples/fig_02_27.go
+++ examples/fig_02_27.go
@@ -20,37 +20,45 @@ func (p Parser) initialize() {
 	p.eq = ""
 }
 
-func (p Parser) getEquation() {
+func (p *Parser) getEquation() {
+	fmt.Println("Please enter the equation with only + and - signs with a single-digit integer")
+	fmt.Printf(">>")
 	fmt.Scanln(&p.eq)
+	// p.eq = "5+4+2+3"
+	fmt.Println(p)
 }
 
-func (p Parser) expr() error {
+func (p *Parser) expr() error {
 	p.term()
 	for j := 1; j > 0; j++ {
 		if p.lookahead >= len(p.eq) {
 			return nil
 		} else if string(p.eq[p.lookahead]) == "+" {
-
+			p.match("+")
+			p.term()
+			fmt.Printf("+")
 		} else if string(p.eq[p.lookahead]) == "-" {
 			p.match("-")
-			p.match(string(p.eq[p.lookahead]))
-
+			p.term()
+			fmt.Printf("-")
 		} else {
 			return fmt.Errorf("syntax error")
 		}
 	}
+	return nil
 }
 
-func (p Parser) term() error {
+func (p *Parser) term() error {
+
 	if _, err := strconv.Atoi(string(p.eq[p.lookahead])); err != nil {
 		return fmt.Errorf("syntax error")
 	}
-	fmt.Println(p.eq[p.lookahead])
+	fmt.Printf(string(p.eq[p.lookahead]))
 	p.match(string(p.eq[p.lookahead]))
 	return nil
 }
 
-func (p Parser) match(c string) error {
+func (p *Parser) match(c string) error {
 	if c != string(p.eq[p.lookahead]) {
 		return fmt.Errorf("syntax error")
 	}
@@ -59,9 +67,9 @@ func (p Parser) match(c string) error {
 }
 
 func main() {
-	var p Parser
+	var q Parser
+	p := &q
 	p.initialize()
 	p.getEquation()
 	p.expr()
-	fmt.Println('\n')
 }
