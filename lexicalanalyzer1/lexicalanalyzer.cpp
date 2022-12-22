88bae205275180eb0f95bff9719626a2ba164800
root <juneho.bang85@gmail.com>
2022-12-22 15:54:22 +0900

    lexical analyzer

--- lexicalanalyzer1/lexicalanalyzer.cpp
+++ lexicalanalyzer1/lexicalanalyzer.cpp
@@ -4,31 +4,26 @@
 #include "lexicalanalyzer.h"
 #include <map>
 
-map<string, TOKEN> words;
+map<string, TOKEN*> words;
 
 int main(){                     //This is the lexical analyzer
     string input = "2+a+42+3";
     int peek=0;
-    TOKEN t;
+    TOKEN* t;
 
     TOKEN reserved;
-    reserved.tag = TRUE;
-    reserved.lexeme = "true";
+    // reserved.tag = TRUE;
+    // reserved.lexeme = "true";
     while(peek < input.size()){
         t = ScanToken(input, &peek);
-        if(t.tag == NUM)
-            cout << "<NUM," <<  t.value << "> ";
-        else if(t.tag == ID)
-            cout << "<ID," << t.lexeme << "> ";
-        else 
-            cout << "<" <<  char(t.tag) << "> ";
+        t->printContent();
     }  
 
     return 0;
 }
 
-TOKEN ScanToken(string input, int* peek){
-    TOKEN t;
+TOKEN* ScanToken(string input, int* peek){
+    TOKEN* t;
 
     while(*peek<input.length()){
         if(input[*peek]==' '|| input[*peek]=='\t')
@@ -45,8 +40,7 @@ TOKEN ScanToken(string input, int* peek){
             v = v*10 + ((int)input[*peek] - 48); // digits in ASCII code begins with 48. That is, '0'...'9' --> 49 ... 58
             (*peek)++;
         } while(isdigit(static_cast<unsigned char>(input[*peek])));
-        t.tag =NUM;
-        t.value = v;
+        t = new Num(v);
         return t;        
     }
 
@@ -57,26 +51,23 @@ TOKEN ScanToken(string input, int* peek){
             (*peek)++;
         }while(isalpha(input[*peek])||isdigit(static_cast<unsigned char>(input[*peek])));
 
-        map<string, TOKEN>::iterator existing = words.find(s);
+        map<string, TOKEN*>::iterator existing = words.find(s);
         if(existing != words.end()){
             return existing->second;
         } 
-        TOKEN t;
-        t.tag = ID;
-        t.lexeme = s;
-        reserve(s, t);
+        t = new Word(ID, s);
+        reserve(s, t); 
         return t;
     }
     else{
-        Token t;
-        t.tag = (int)(input[*peek]);
+        t = new Token((int)(input[*peek]));
         (*peek)++;
         return  t;
     }
     return t;
 }
 
-void reserve(string lexeme, TOKEN t){
-    words[lexeme] = t;
+void reserve(string s, TOKEN* t){
+    words[s] = t;
     return;
 }
--- lexicalanalyzer1/lexicalanalyzer.h
+++ lexicalanalyzer1/lexicalanalyzer.h
@@ -13,33 +13,45 @@ using namespace std;
 class Token{
     public:
         int tag;
-        int value;
-        string lexeme;
-        
 
-        // Token(int t){
-        //     tag = t;
+        Token(){tag = 0;}
+        Token(int t){
+            tag = t;
         //     value = 0;
         //     lexeme = "";
-        // }
+        }
+        virtual void printContent(){
+            cout << "<" << (char)tag << "> ";
+        }
 };
 
-// class Num:Token{
-//     public:
-//         Num(int v):Token(NUM){
-//             value = v;
-//         }
-// };
+class Num : public Token{
+    public:
+        int value;
 
-// class Word:Token{
-//     public:
-//         Word(int t, string s):Token(t){
-//             lexeme = s;
-//         }
-// };
+        Num(int v):Token(NUM){
+            value = v;
+        }
+        void printContent(){
+            cout <<"<NUM," << value << "> ";
+        }
+};
+
+class Word : public Token{
+    public:
+        string lexeme;
+
+        Word(int t, string s):Token(t){
+            lexeme = s;
+        }
+        void printContent(){
+            cout<< "<ID," << lexeme << "> ";
+        }
+
+};
 
 typedef Token TOKEN;
-// typedef Num NUMV;
-// typedef Word WORDV;
-TOKEN ScanToken(string input, int* peek);
-void reserve(string lexeme, TOKEN t);
+typedef Num NUMV;
+typedef Word WORDV;
+TOKEN* ScanToken(string input, int* peek);
+void reserve(string s, TOKEN* t);
